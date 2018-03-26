d506ad7b1a1878dbe838f7ce001ed6de50136830
rustemRA <rustem_pimp@mail.ru>
2018-03-26 10:20:24 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication12/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace WindowsFormsApplication12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать матрицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.PeachPuff;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Crimson;
+            this.dataGridView1.Location = new System.Drawing.Point(239, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(552, 403);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 179);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Выполнить \r\n  задание";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(46, 356);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 51);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Заполнить матрицу";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 34);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Количество \r\n  столбцов";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 34);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Количество\r\n     строк";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(41, 532);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 50);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Выберите\r\nцвет фона";
+            this.label3.Visible = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.HotPink;
+            this.button8.Location = new System.Drawing.Point(355, 555);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(27, 27);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "\r\n";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkViolet;
+            this.button7.Location = new System.Drawing.Point(304, 556);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(26, 27);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "\r\n";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button6.Location = new System.Drawing.Point(247, 556);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(27, 27);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "\r\n";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.GreenYellow;
+            this.button5.Location = new System.Drawing.Point(355, 523);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 26);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "\r\n";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(304, 523);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 27);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "\r\n";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.Location = new System.Drawing.Point(247, 523);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(27, 27);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "\r\n";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mistral", 20F);
+            this.label5.Location = new System.Drawing.Point(915, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 41);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Тюлемисова Дана";
+            this.label5.Visible = false;
+            // 
+            // button10
+            // 
+            this.button10.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button10.BackColor = System.Drawing.Color.Red;
+            this.button10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button10.Font = new System.Drawing.Font("Monotype Corsiva", 25.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.ForeColor = System.Drawing.Color.Gold;
+            this.button10.Location = new System.Drawing.Point(172, 145);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(274, 90);
+            this.button10.TabIndex = 21;
+            this.button10.Text = "start";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(46, 268);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(162, 51);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "Создать матрицу";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Visible = false;
+            this.button11.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.PeachPuff;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.Color.Crimson;
+            this.dataGridView2.Location = new System.Drawing.Point(239, 94);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(552, 403);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.Visible = false;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(46, 445);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(162, 52);
+            this.button12.TabIndex = 6;
+            this.button12.Text = "Выполнить \r\n  задание";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Visible = false;
+            this.button12.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(46, 356);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(162, 51);
+            this.button13.TabIndex = 9;
+            this.button13.Text = "Заполнить матрицу";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Visible = false;
+            this.button13.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 34);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Количество\r\n     строк";
+            this.label4.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Mistral", 20F);
+            this.label6.Location = new System.Drawing.Point(915, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 41);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Тюлемисова Дана";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(870, 609);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Тюлемисова Дана";
+            this.Activated += new System.EventHandler(this.Form1_Activated_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApplication12/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            textBox1.Text = "15";
+            textBox2.Text = "15";
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label6.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            button9.Visible = false;
+            button10.Visible = true;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            dataGridView1.Visible = false;
+            this.Size = new Size(460, 300);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            int y = Convert.ToInt32(textBox2.Text);
+            dataGridView1.RowCount = y;
+            dataGridView1.ColumnCount = x;
+            for (int i = 0; i < x; i++)
+            {
+                dataGridView1.Columns[i].Width = 24;
+            }
+            for (int i = 0; i < y; i++)
+            {
+                dataGridView1.Rows[i].Height = 20;
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   int x = Convert.ToInt32(textBox1.Text);
+            int y = Convert.ToInt32(textBox2.Text);
+            int[,] a = new int[x, y]; int sum = 0;
+            for (int j = 0; j < y; j++)
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    a[i, j] = int.Parse(dataGridView1[i,j].Value.ToString());
+                  
+                    if (a[0, j] == 1)
+                    {
+                          sum +=  a[i, j];
+                    } 
+                } if (a[0, j] == 1)
+                {
+                    MessageBox.Show(Convert.ToString("средне арифметическое значение = " + sum/x), 
+                        "Тюлемисова Дана",MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+                }sum = 0;
+            } 
+            for (int j = 0; j < x; j++)
+            {
+                if (a[0, j] == 1)
+                {MessageBox.Show("Строка - " + Convert.ToString(j + 1) + " начинается с единицы", 
+                    "Тюлемисова Дана", MessageBoxButtons.OKCancel, MessageBoxIcon.Information); }
+                else
+                { MessageBox.Show(Convert.ToString(j + 1) + " - строка начинается не с единицы",
+                    "Тюлемисова Дана", MessageBoxButtons.OKCancel, MessageBoxIcon.Error); }}}
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {  int x = Convert.ToInt32(textBox1.Text);
+            int y = Convert.ToInt32(textBox2.Text);
+            Random Ra = new Random();
+            for (int w = 0; w < x; w++)
+            {
+                for (int r = 0; r < y; r++)
+                { dataGridView1[w, r].Value = Ra.Next(-50, 50); }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.GreenYellow;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DodgerBlue;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkViolet;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.HotPink;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label6.Left < this.Width)
+            { label6.Left += 2; }
+            else
+            { label6.Left = -label6.Width; }
+
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Form1_Activated_1(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label6.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            button8.Visible = true;
+            button9.Visible = true;
+            button10.Visible = false;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            dataGridView1.Visible = true;
+            timer1.Start();
+            this.Size = new Size(650, 550); 
+                   }
+    }
+}
